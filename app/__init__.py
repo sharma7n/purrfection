8c69d86be850bce2d01c68694088b48ea997a05a
sharma7n <sharma7n@gmail.com>
2016-08-20 15:24:55 -0500

    more envvar

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 # purrfection/__init__.py
 # app initializer
 
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_envvar('SECRET_CONFIG')
-# app.config.from_pyfile('config.py')
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 db = SQLAlchemy(app)
 
 from .users import signup, confirm, login, logout
